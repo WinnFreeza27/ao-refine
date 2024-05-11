b9464c10093784e07f734e4ca2615cb9899b2a6d
WinnFreeza27 <72385674+WinnFreeza27@users.noreply.github.com>
2024-05-11 21:13:11 +0800

    Delete src/ui-component directory
    Changed name ui-components to components

--- src/ui-component/backgroundGradient.jsx
+++
@@ -1,7 +0,0 @@
-export default function Background({child}) {
-    return (
-    <div className="bg-gradient-to-br from-bg-purple from-10% to-bg-purple-dark to-90% w-screen h-screen">
-        {child}
-    </div>
-    )
-}
--- src/ui-component/blackOverlay.jsx
+++
@@ -1,5 +0,0 @@
-export default function Blackoverlay() {
-    return(
-        <div className="absolute z-10 w-screen h-full bg-black/80 top-0"></div>
-    )
-}
