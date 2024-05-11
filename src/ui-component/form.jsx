87baec686fe165215d873cffa2b0b8976cd206e1
WinnFreeza27 <erwinpratamaeyr7@gmail.com>
2024-05-11 21:38:22 +0800

    delete changing name folder

--- src/ui-component/form.jsx
+++
@@ -1,7 +0,0 @@
-export default function Form({placeholder,inputType, id, styleClass, noDefaultClass}) {
-    return(
-        <>
-            <input type={inputType} id={id} name={id} className={`outline-none ${noDefaultClass ? "" : "input-form"} ${styleClass}`} placeholder={placeholder}/>
-        </>
-    )
-}
